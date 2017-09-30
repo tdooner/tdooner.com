6e6779e36c4b33516c0b9b896a9e128e96f783b1
Tom Dooner <tomdooner@gmail.com>
2017-09-30 16:45:47 -0700

    Update Resume for post-CfA life
    A few things:
    
    1) forked the jsonresume-theme-short to make it a bit more compact and
    tweak some visual styling things
    2) updated the descriptions of my jobs to be more timely
    3) switched rendering engine to Headless Chrome w/Puppeteer, which is
    way easier to control than wkhtmltopdf, somehow faster, and produces a
    better result

--- Makefile
+++ Makefile
@@ -2,5 +2,4 @@ deploy:
 	bundle exec middleman deploy
 
 build/resume.pdf: source/resume.json Makefile
-	hackmyresume BUILD source/resume.json TO build/resume.all -t node_modules/jsonresume-theme-short
-	/usr/local/bin/wkhtmltopdf --margin-top 20mm --margin-bottom 0mm --zoom 0.75 build/resume.pdf.html build/resume.pdf
+	hackmyresume BUILD source/resume.json TO build/resume.pdf.html -t jsonresume-theme-short-tdooner
---
+++ build-resume.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+const RESUME_PATH =
+  'file:///Users/tomdooner/dev/ruby/tdooner.com/build/resume.pdf.html';
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(RESUME_PATH, { waitUntil: 'load' });
+  await page.waitForFunction(() => document.fonts.status === 'loaded');
+  await page.pdf({
+    path: 'build/resume.pdf',
+    format: 'Letter',
+  });
+  console.log('Done!');
+
+  browser.close();
+})();
